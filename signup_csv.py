f66feec300d957a38687c2ccf115b65e6cb9a706
Bosco Lau <bosco.kc.lau@gmail.com>
2020-07-09 20:52:47 -0700

    Restructured code to make it testable. Added ProgramStatus, for consistency with other parts of the code. Will replace status code probably with exceptions/errors

--- signup_csv.py
+++ signup_csv.py
@@ -10,56 +10,64 @@ import os
 import sys
 import argparse
 import mass_signup
+from constants import ProgramStatus
 from buyer import Buyer
 from attendee import Attendee
 
 # Application settings
 MAX = 10
 
-parser = argparse.ArgumentParser()
-parser.add_argument('event_url', help='URL of the EventBrite event.')
-parser.add_argument('csv_path', help='File path to the attendee CSV file.')
-parser.add_argument('-c', '--command-line-mode', dest='headless', help='Command line mode. Run browser headless.',
-                    action='store_true')
-args = parser.parse_args()
 
-event_url = args.event_url
-csv_path = args.csv_path
-headless = args.headless
+def process_registration(event_url: str, csv_path: str, headless: bool = False):
+    if not os.path.isfile(csv_path):
+        print("csv file does not exist.")
+        return ProgramStatus.FILE_NOT_FOUND
 
-if not os.path.isfile(csv_path):
-    sys.exit("csv file does not exist.")
+    # Buyer is always the OLMV Mass EB organizer
+    buyer_FN = "OLMV"
+    buyer_LN = "Seattle"
+    buyer_email = "olmv.seattle@gmail.com"
 
-# Buyer is always the OLMV Mass EB organizer
-buyer_FN = "OLMV"
-buyer_LN = "Seattle"
-buyer_email = "olmv.seattle@gmail.com"
+    buyer = Buyer(buyer_FN, buyer_LN, buyer_email)
 
-buyer = Buyer(buyer_FN, buyer_LN, buyer_email)
+    attendee_list = list()
 
-attendee_list = list()
+    with open(csv_path) as f:
+        attendee_lines = f.read().splitlines()
 
-with open(csv_path) as f:
-    attendee_lines = f.read().splitlines()
+    # Check with MAX + 1 as the first line is headers
+    if len(attendee_lines) > MAX + 1:
+        print(f"Requests exist maximum allowed. Max = {MAX}")
+        return ProgramStatus.CONSTRAINT_VIOLATION
 
-# Check with MAX + 1 as the first line is headers
-if len(attendee_lines) > MAX + 1:
-    sys.exit(f"Requests exist maximum allowed. Max = {MAX}")
+    # Remove header line=
+    attendee_lines.pop(0)
 
-# Remove header line=
-attendee_lines.pop(0)
+    for line in attendee_lines:
+        attendee = Attendee.from_csv_string(line)
+        # print( attendee )
+        attendee_list.append(attendee)
 
-for line in attendee_lines:
-    attendee = Attendee.from_csv_string(line)
-    # print( attendee )
-    attendee_list.append(attendee)
+    print("Processing:")
+    print("\tAttendee file:", csv_path)
+    print("\tEvent: ", event_url)
+    print()
 
-print("Processing:")
-print("\tAttendee file:", csv_path)
-print("\tEvent: ", event_url)
-print()
+    return mass_signup.signup(attendee_list, buyer, event_url, headless)
 
-mass_signup.signup(attendee_list, buyer, event_url, headless)
 
-if __name__ != '__main__':
-    sys.exit("File as module not supported")
+# Program Main
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('event_url', help='URL of the EventBrite event.')
+    parser.add_argument('csv_path', help='File path to the attendee CSV file.')
+    parser.add_argument('-c', '--command-line-mode', dest='headless', help='Command line mode. Run browser headless.',
+                        action='store_true')
+    args = parser.parse_args()
+
+    event_url = args.event_url
+    csv_path = args.csv_path
+    headless = args.headless
+
+    status = process_registration(event_url, csv_path, headless)
+    sys.exit(status)
